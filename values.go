2f61e62fd7d5f9a750547f9227911fbbe71ec80d
sfzhang <andy.ledway3@gmail.com>
2017-01-27 21:46:15 -0500

    adding code

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    fmt.Println("2","+","3","is",2+3,"\n")
+    fmt.Println("True","||","False","is",true || false,"\n")
+    fmt.Println("10","/","3","is",10/3,"\n")
+    fmt.Println("3","*","4","is",3*4,"\n")
+    fmt.Println("True","&&","True","is",true && true,"\n")
+    fmt.Println("not","True","is",!true,"\n")
+    fmt.Println("shaofeng"+"zhang","\n")
+
+}
---
+++ variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    var e int
+    fmt.Println(e)
+    
+    var a int = 5
+    fmt.Println(a)
+
+    var c,d float32 = 5, 7.8
+    fmt.Println(c,d)
+
+    var f bool
+    fmt.Println(f)
+
+    x:=100.2323
+    fmt.Println(x)
+
+   
+}
