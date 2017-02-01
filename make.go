b5ff7a232ced58660e79234b7d4dc3a8b967cf60
sfzhang <andy.ledway3@gmail.com>
2017-01-31 23:59:49 -0500

    adding make.go illurstrating reference type(slice mapping)

---
+++ make.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	
+
+	a := make(map[string]string)[
+	
+		"id": "1004",
+		"name": "shaofeng zhang",]
+	
+	b := make([]string,3)
+	b = ["alex","helen","nova"]
+	
+	c:= make(chan int,4)
+	
+	fm.Printf("Before: %v-----%v------%v",a,b,c)
+	make(a, b, c)
+	fm.Printf("After: %v-----%v------%v",a,b,c)
+	
+	d := <-c
+         
+
+
+
+
+
+}
+
+
+func make(x []string, y [string]string, z chan int){
+
+	x["address"] = "404 55 Oakmount Rd."
+
+	y = append(y, "john")
+
+
+        z<-1
+
+
+
+
+}
