83cc5b0a3b1dc281cf41c2a8a838e9a757236fb5
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-02-04 16:30:17 -0500

    mutable slice

---
+++ slice_caveat.go
@@ -0,0 +1,58 @@
+package main
+import "fmt"
+
+func modify1(slice *[]int){
+    	*slice = (*slice)[:3]
+	fmt.Println("within modify1:", *slice)
+
+}
+
+
+func modify2(slice []int){
+        fmt.Printf("%p\n", &(slice[1]))
+    	slice = slice [:3]
+	fmt.Printf("%p\n", &(slice[1]))
+	fmt.Println("within modify2:", slice)
+
+}
+
+func modify3(slice []int){
+    	slice[0] = 3333
+	fmt.Println("within modify3:", slice)
+
+
+}
+
+func main(){
+    	original1:= []int {1, 2, 3, 4, 5}
+   	original2:= []int {1, 2, 3, 4, 5}
+   	original3:= []int {1, 2, 3, 4, 5}
+
+  	modify1(&original1)
+   	fmt.Println("original1: = ", original1)
+
+   	modify2(original2)
+    	fmt.Println("original2: = ", original2)
+
+    	modify3(original3)
+    	fmt.Println("original3: = ", original3)
+
+
+}
+/*
+Result:
+within modify1: [1 2 3]
+original1: =  [1 2 3]
+0x10432224
+0x10432224
+within modify2: [1 2 3]
+original2: =  [1 2 3 4 5]
+within modify3: [3333 2 3 4 5]
+original3: =  [3333 2 3 4 5]
+
+In my 3 modify functions, the second does not change the original slice,
+the other two changes the underlying data, in the second modify
+we just using a local variable slice assigned to point to the original slice
+it has no effect on the original one
+
+/*
