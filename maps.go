070ec37b6e46f93c783dcdee026b3be7c2077846
Shaofeng Zhang <andy.ledway3@gmail.com>
2017-02-22 20:12:03 -0500

    finished exercise 4.8 4.9

--- maps.go
+++ maps.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"os"
+	"bufio"
+
+	"unicode"
 )
 
-func main() {
-
+func map_test(){
 	m := make(map[string]int)
 	m["age"] = 23
 	m["id"] = 1009
@@ -26,6 +30,7 @@ func main() {
 	m2 = make(map[string]string)
 	m2["name"] = "Shaofeng zhang"
 	m2["address"] = "404 55 Oakmount Road."
+
 
 	fmt.Println(m2)
 
@@ -50,5 +55,92 @@ func main() {
 		fmt.Println(idx)
 
 	}
+
+}
+
+func WordFreq(file string){
+
+	wordcount := make(map[string]int)
+	f, success := os.Open(file)
+	if success != nil{
+		fmt.Fprintf(os.Stderr, "%v while open %v", success, file)
+		os.Exit(1)
+	}
+
+	// note that we can not use
+	// scanner :=  &bufio.Scanner{
+	//                      r:f,
+	//                      split:bufio.ScanWords,
+	//                      MaxTokenSize: bufio.MaxScanTokenSize,
+	//     }
+	// to initialize our scanner because bufio.r bufio.split...
+	// can not be exported, we need to  use its exported method
+	// bufio.NewScanner method, if we want to change its split fun
+	// we use the exported bufio.ScanWords
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanWords)
+
+	for {
+		if scanner.Scan(){
+			wordcount[scanner.Text()]++
+		}else{
+			break
+		}
+
+        for i,j :=range wordcount{
+
+		fmt.Printf("%v----%d\n",i,j)
+	}
+
+	}
+
+
+
+
+
+}
+
+
+func CharCount(file string){
+        count := make(map[string]int)
+	f, status := os.Open(file)
+	if status != nil{
+		fmt.Fprintf(os.Stderr, "can not open %v, error:%v", file,status)
+	}
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanRunes)
+	for {
+		if scanner.Scan(){
+			r := []rune(scanner.Text())[0]
+			switch {
+				case unicode.IsSymbol(r): count["symbol"]++
+				case unicode.IsSpace(r): count["space"]++
+				case unicode.IsNumber(r): count["number"]++
+				case unicode.IsDigit(r): count["digit"]++
+				case unicode.IsPunct(r): count["punct"]++
+				case unicode.IsPrint(r): count["print"]++
+				case unicode.IsControl(r): count["control"]++
+			}
+
+		}else{
+			break
+		}
+
+
+	}
+
+	for i,j :=range count{
+
+		fmt.Printf("%v----%d\n",i,j)
+	}
+
+}
+
+
+
+func main() {
+	WordFreq("I:/learning/EnglishLearning/GRE/English+英文原着500本/English 英文原著shu/a message from the sea(大海来信).txt")
+	CharCount("I:/learning/EnglishLearning/GRE/English+英文原着500本/English 英文原著shu/a message from the sea(大海来信).txt")
 
 }
