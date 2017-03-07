cb795d1f14115d5fe7ddb3ecd3b8d8f189205a9b
nill-zhang <andy.ledway3@gmail.com>
2017-03-07 11:25:16 -0500

    adding Join  which simulates strings.join()

--- variadic.go
+++ variadic.go
@@ -30,7 +30,7 @@ func Min(lst ...int32) int32{
 	return min
 }
 
-func Max(lst ...float32) float32{
+func Max(lst ...int32) int32{
 	if len(lst) < 1{
 		return -1.0
 	}
@@ -45,18 +45,51 @@ func Max(lst ...float32) float32{
 
 }
 
-func Sum(lst  ...uint32) uint32{
-	total := 0
+func Sum(lst  ...int32) int32{
+	total := int32(0)
 	for i:=0;i<len(lst);i++{
 		total += lst[i]
 
 	}
-return  total
+	return  total
+}
+
+// note that the following commented func declaration is wrong
+// ... can only be used as the final parameter
+// func Join(strs ...string, sep string)string{
+func Join(sep string, strs ...string) (res string){
+	if len(strs) == 0{
+		res = sep
+		return
+	}
+	res = ""
+	for _, value := range strs[:len(strs) - 1] {
+		res += value + sep
+	}
+	res =  res + strs[len(strs)-1]
+	return
 }
 
 
-func main() {
+func Join_test(){
+	p := fmt.Println
+	p(Join("|","Alex","is","a","good","boy!"))
+	p(Join("|",[]string{"Alex","is","a","good","boy!"}...))
+	p(Join("",[]string{"Alex","is","a","good","boy!"}...))
+	p(Join("",[]string{}...))
+	p(Join("|",[]string{}...))
+	p(Join("",[]string{"Oneword"}...))
+}
 
+func ArithFunc_test(){
+	var list = []int32{2,2,3,12,-11,-2,232,221,102}
+	// behaves like python sequence unpacking
+	fmt.Printf("Sum(%v) = %v\n", list, Sum(list...))
+	fmt.Printf("Min() = %v\n",Min(2,2,2,-1,0,-23,2323))
+	fmt.Printf("Max() = %v\n",Max(2,2,2,-1,0,-2343,2323))
+}
+
+func CustomAppend_test(){
 	var  original = []int{1,1,1,1,1,1,1}
 	var  more = []int{2,2,2,2,2,2,2}
 	// note that here if is a slice using ... after the slice parameter
@@ -65,7 +98,12 @@ func main() {
 	fmt.Println(original)
 	original = custom_append(original, 3,3,3,3,3,3,3)
 	fmt.Println(original)
+}
+func main() {
 
+	Join_test()
+	//ArithFunc_test()
+	//CustomAppend_test()
 }
 
 // Note: for functions that take or return any type argument
