473212c2f5547748165d1130b40574ac2d5ed951
nill-zhang <andy.ledway3@gmail.com>
2017-03-08 22:52:16 -0500

    finished other set operations AddAll, Difference, SymmericDifference, Intersection and test code

--- intset.go
+++ intset.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"strconv"
 	"fmt"
+	"text/tabwriter"
+	"os"
+	"strings"
 )
 
 type IntSet struct{
@@ -17,6 +20,7 @@ func (s *IntSet)Len() uint64{
 
 }
 
+
 func (s *IntSet)UnionWith(snd IntSet){
 
 	if len(s.set) <= len(snd.set){
@@ -29,27 +33,67 @@ func (s *IntSet)UnionWith(snd IntSet){
 
 	for i:=0;i<len(snd.set);i++{
 			s.set[i] = s.set[i] | snd.set[i]
+		        // keep the rest s.set[len(snd):]
+
+		}
+}
+
+
+func (s *IntSet)IntersectWith(snd IntSet){
+	if len(s.set) <= len(snd.set){
+		for i:=0;i<len(s.set);i++{
+			s.set[i] = s.set[i] & snd.set[i]
+
+		}
+	}
+
+	for i:=0;i<len(snd.set);i++{
+			s.set[i] = s.set[i] & snd.set[i]
+		        // truncate the set
+		        s.set = s.set[:len(snd.set)]
+
+		}
+}
+
+
+func (s *IntSet)DifferenceWith(snd IntSet){
+
+	if len(s.set) <= len(snd.set){
+		for i:=0;i<len(s.set);i++{
+			s.set[i] = s.set[i] &^ snd.set[i]
+
+		}
+	}
+	for i:=0;i<len(snd.set);i++{
+			s.set[i] = s.set[i] &^ snd.set[i]
+		        // keep the rest s.set[len(snd.set):]
+		}
+}
+
+
+func (s *IntSet)SymmetricDifference(snd IntSet){
+	if len(s.set) <= len(snd.set){
+		for i:=0;i<len(s.set);i++{
+			s.set[i] = (s.set[i] &^ snd.set[i]) |  (snd.set[i] &^ s.set[i])
 
 		}
 
+		s.set = append(s.set, snd.set[len(s.set):]...)
+	}
 
-
-
+	for i:=0;i<len(snd.set);i++{
+			s.set[i] = (s.set[i] &^ snd.set[i]) |  (snd.set[i] &^ s.set[i])
+		        // keep the rest s.set[len(snd.set):]
+		}
 }
 
-func (*IntSet)IntersectWith(){
 
-
-}
-
-func (*IntSet)DifferenceWith(){
-
-
-}
-
-func (*IntSet)AddAll(sets ...IntSet){
-
-
+func (s *IntSet)AddAll(sets ...IntSet){
+	for _,j := range sets{
+		for _,k := range j.Elem(){
+			s.Add(k)
+		}
+	}
 }
 
 
@@ -59,28 +103,22 @@ func (s *IntSet)Remove(nm uint64) bool{
 		s.set[nm/64] = s.set[nm/64] &^ (uint64(1)<<(nm%64))
 		return true
 	}
-
-
 	return false
 }
+
 
 func (s *IntSet)Clear(){
 
 	for i:=0;i<len(s.set);i++{
 		s.set[i] = s.set[i] & uint64(0)
 	}
-
-
-
 }
 
 
-func (*IntSet)Copy(){
-
-
-
-
-
+func (s *IntSet)Copy() *IntSet{
+	cp := make([]uint64, len(s.set))
+	copy(cp, s.set)
+	return &IntSet{cp}
 
 }
 
@@ -90,11 +128,8 @@ func (s *IntSet)Elem() (res []uint64){
 		for j:=0;j<64;j++{
 			if (uint64(1)<<uint64(j) & s.set[i]) != 0{
 				res = append(res, uint64(i*64+j))
-
 			}
-
 		}
-
 	}
 
 	return
@@ -104,19 +139,13 @@ func (s *IntSet)Elem() (res []uint64){
 
 func (s *IntSet)Add(nu uint64){
 
-	//fmt.Printf("%d\n",s.Len())
-	//fmt.Printf("%d\n",len(s.set))
-	//fmt.Printf("%d\n",nu/64)
 
 	if nu/64 >= uint64(len(s.set)){
 		y := make([]uint64, (nu/64)+1-uint64(len(s.set)))
 		s.set = append(s.set, y...)
-		//fmt.Printf("len(s.set):    %d\n",len(s.set))
-
 
 	}
 	s.set[nu/64] = s.set[nu/64] | (uint64(1)<<(nu%64))
-	//fmt.Printf("s.set[%v]:    %d\n",nu/64, s.set[nu/64])
 
 
 }
@@ -136,24 +165,21 @@ func (s *IntSet)Has(nu uint64) bool{
 }
 
 
-func (s *IntSet)SymmetricDifference(){
 
-
-
-}
 
 func (s *IntSet)String() string{
 
 	var buf bytes.Buffer
-	_, err := buf.Write([]byte("{"))
-	//fmt.Printf("s.Elem(): %v\n", s.Elem())
+	elements := []string{}
+	//_, err := buf.Write([]byte("{"))
 	for _,j := range s.Elem(){
 
-		_, err = buf.Write([]byte(strconv.FormatUint(j,10)))
-		_, err = buf.Write([]byte(","))
+		elements = append(elements, strconv.FormatUint(j,10))
+		//_, err = buf.Write([]byte(strconv.FormatUint(j,10)))
+		//_, err = buf.Write([]byte(","))
 	}
 
-	_, err = buf.Write([]byte("}"))
+	_, err := buf.Write([]byte("{"+strings.Join(elements,",")+"}"))
 
 	if err != nil{
 
@@ -163,7 +189,44 @@ func (s *IntSet)String() string{
 
 }
 
-func main(){
+
+func SetOperation_test(){
+	myset := &IntSet{}
+	myset.Add(uint64(6))
+	myset.Add(uint64(66))
+	myset.Add(uint64(666))
+	myset.Add(uint64(6666))
+	myset.Add(uint64(3))
+
+	var herset =&IntSet{}
+	herset.Add(uint64(6))
+	herset.Add(uint64(10))
+	herset.Add(uint64(100))
+	herset.Add(uint64(18))
+	herset.Add(uint64(6666))
+
+	s1 := myset.Copy()
+	s2 := myset.Copy()
+	s3 := myset.Copy()
+	s4 := myset.Copy()
+
+	s1.UnionWith(*herset)
+	s2.IntersectWith(*herset)
+	s3.DifferenceWith(*herset)
+	s4.SymmetricDifference(*herset)
+	fmt.Printf("myset:\t%v\t",myset)
+	fmt.Printf("herset:\t%v\n",herset)
+	w := tabwriter.NewWriter(os.Stdout,40,0,0,' ',0)
+	fmt.Fprintf(w,"\033[1;34m%v\t%v\t%v\t%v\t\n\033[0m","Union","Intersection","Difference","SymmetricDifference")
+	fmt.Fprintf(w,"%v\t%v\t%v\t%v\t\n",s1,s2,s3,s4)
+	w.Flush()
+
+
+}
+
+
+func SetOperationOther_test(){
+
 	p := fmt.Printf
 	//var myset *IntSet
 	myset := &IntSet{}
@@ -200,7 +263,20 @@ func main(){
 	myset.Clear()
 	p("After Clear() myset: %v\n", myset)
 
+	var set1 = &IntSet{}
+	var set2 =&IntSet{}
+	var set3 =&IntSet{}
+	set1.Add(uint64(5))
+	set2.Add(uint64(74))
+	set3.Add(uint64(1002))
+	myset.AddAll(*set1,*set2,*set3)
+	p("myset: %v\n", myset)
+}
+func main(){
 
+
+	SetOperation_test()
+	SetOperationOther_test()
 
 
 
