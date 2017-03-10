7eec6f2a5754112d7c52d4374c0932e5482974c9
nill-zhang <andy.ledway3@gmail.com>
2017-03-09 20:49:27 -0500

    adding custom MyReader type mimics strings.NewReader

--- ioreader.go
+++ ioreader.go
@@ -1,5 +1,7 @@
 package main
-import ("fmt";"io";"strings";"time")
+import ("fmt";"io";"strings";"time"
+
+)
 
 
 type Emit struct{
@@ -46,9 +48,93 @@ func test_Custom_ioReader(){
 
 
 }
+
+
+type Rd struct{
+	        str string
+		cur int
+	}
+
+func(R *Rd) Read(p []byte)(m int, err error){
+		if len(p) + R.cur <= len(R.str) {
+			m = copy(p, []byte(R.str[R.cur:len(p) + R.cur]))
+			R.cur = R.cur + m
+			err = nil
+			return
+		}
+		m = copy(p, []byte(R.str[R.cur:]))
+		err = io.EOF
+		return
+	}
+
+
+func MyReader(s string) io.Reader{
+
+	r := Rd{s,0}
+	return &r
+}
+
+
+func MyReader_test(){
+	y := MyReader("This is freaking awesome!!")
+	p := make([]byte, 4)
+	for{
+		n,err := y.Read(p)
+		// you should not print n-p pair after if-statement
+		// because you get out of the loop early and leave out the final
+		// pair
+		fmt.Printf("%d bytes read, Buffer: %v\n",n,string(p))
+		if err != nil{
+			break
+	}
+
+	}
+}
+
+//func LimitReader(r io.Reader, n int) (newr io.Reader){
+//	//p := make([]byte, 1)
+//	total := 0
+//	func (newr io.Reader) Read(p []byte)(m int, err error){
+//		m,err = r.Read(p)
+//		total += m
+//		if total > n {
+//			m = m-(total-n)
+//			err = io.EOF
+//			return
+//		}
+//		return
+//
+//
+//	}
+//	return
+//
+//
+//}
+
+
+
+//func LimitReader_test(){
+//	r := strings.NewReader("woshiyigefeichangliaobuqideren")
+//	newr := LimitReader(r,7)
+//	p := make([]byte, 3)
+//
+//	for {
+//		nbytes, err := newr.Read(p)
+//		if err!= nil{
+//			break
+//		}
+//		fmt.Printf("%v bytes read, Buffer:%v\n",nbytes,string(p))
+//
+//	}
+//
+//
+//}
+
 func main() {
 
-	test_Custom_ioReader()
-	test_ioReader()
+	//test_Custom_ioReader()
+	//test_ioReader()
+	MyReader_test()
+	//LimitReader_test()
 
 }
