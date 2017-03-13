83e6975d3b19261ab415d026e103d4362507d940
nill-zhang <andy.ledway3@gmail.com>
2017-03-13 04:23:37 -0400

    finished BinarySearchTree package and its testing code

--- tree.go
+++ tree.go
@@ -5,15 +5,15 @@ import (
 	"fmt"
 )
 
-type BinaryTree struct{
+type BinarySearchTree struct{
 
 	value int
-        lchild,rchild,parent,sibling *BinaryTree
+        lchild,rchild,parent,sibling *BinarySearchTree
 }
 
 // InitFromSlice takes a slice and add all the elements to the tree
 // the first element will be the root node
-func (b *BinaryTree)InitFromSlice(s []int)error {
+func (b *BinarySearchTree)InitFromSlice(s []int)error {
 	if len(s) < 1 {
 		return errors.New("no elements to initialize from")
 	}
@@ -26,7 +26,7 @@ func (b *BinaryTree)InitFromSlice(s []int)error {
 
 // Sort will output the tree as a sorted slice
 // breadth-firt leftchild-->value-->rightchild
-func (b *BinaryTree)Sort()(sorted []int){
+func (b *BinarySearchTree)Sort()(sorted []int){
 	if b.lchild != nil {
 		sorted = append(sorted,b.lchild.Sort()...)
 	}
@@ -41,17 +41,15 @@ func (b *BinaryTree)Sort()(sorted []int){
 
 }
 
-func (b *BinaryTree)Add(values ...int){
-
-	for _,elemvalue := range values {
+func (b *BinarySearchTree)Add(elemvalue int){
 		if b.value > elemvalue {
 			if b.lchild != nil {
 				b.lchild.Add(elemvalue)
 				return
 			}
-                        // if no left child present, then contruct a leftchild and
+                        // if no left child present, then construct a left child and
 			// set its value as elemvalue, its parent as b
-			newnode := &BinaryTree{value:elemvalue,parent:b, sibling:b.rchild}
+			newnode := &BinarySearchTree{value:elemvalue,parent:b, sibling:b.rchild}
 			b.lchild = newnode
 			return
 		}
@@ -59,15 +57,22 @@ func (b *BinaryTree)Add(values ...int){
 			b.rchild.Add(elemvalue)
 			return
 		}
-		newnode := &BinaryTree{value:elemvalue,parent:b,sibling:b.lchild}
+		newnode := &BinarySearchTree{value:elemvalue,parent:b,sibling:b.lchild}
 		b.rchild = newnode
-	}
 }
 
+func (b *BinarySearchTree)AddAll(elems []int){
+
+	for _,j := range elems{
+		b.Add(j)
+	}
+
+
+}
 
 // Has checks whether the tree has an element node whose value matches the given parameter
 // depth-first node-->lchild--->rchild
-func (b *BinaryTree)Has(num int)bool{
+func (b *BinarySearchTree)Has(num int)bool{
 
 	if b.value == num{
 		return true
@@ -85,22 +90,39 @@ func (b *BinaryTree)Has(num int)bool{
 
 }
 
-func BinaryTreeInitial_test(){
+func BinarySearchTreeInitial_test(){
 
-	bt := &BinaryTree{34,nil,nil,nil,nil}
+	bt := &BinarySearchTree{34,nil,nil,nil,nil}
 	bt.InitFromSlice([]int{-21,-1,-5,-8,-17,20,3,9,16,13,2,-76,788,28,-22})
 	fmt.Printf("sorted: %v\n",bt.Sort())
-	bt.Add([]int{56,78,-222}...)
+	bt.AddAll([]int{56,78,-22,-46,304})
 	fmt.Printf("sorted: %v\n",bt.Sort())
 	fmt.Printf("bt has 56? %t\n",bt.Has(56))
 	fmt.Printf("bt has 78? %t\n",bt.Has(78))
 	fmt.Printf("bt has 65? %t\n",bt.Has(65))
 	fmt.Println(bt)
 
-}
+	                  //               /\
+	                  //              //\\
+	                  //             ///\\\
+	                  //            ////\\\\
+	                  //           /////\\\\\
+	                  //          //////\\\\\\
+	                  //         ///////\\\\\\\
+	                  //        ////////\\\\\\\\
+	                  //       /////////\\\\\\\\\
+	                  //      //////////\\\\\\\\\\
+	                  //     ///////////\\\\\\\\\\\
+	                  //    ////////////\\\\\\\\\\\\
+	                  //   /////////////\\\\\\\\\\\\\
+	                  //  //////////////\\\\\\\\\\\\\\
+	                  // ////////////////\\\\\\\\\\\\\\
+	                  ///////////////////\\\\\\\\\\\\\\\
+
+ }
 
 
 
 func main() {
-	BinaryTreeInitial_test()
+	BinarySearchTreeInitial_test()
 }
